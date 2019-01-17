d7102d2ebaeabcc3b71ca78d45c65aab4070b0bf
Stephen Rathbone <stephenr@cpwonlinesolutions.com>
2019-01-17 15:20:30 +0000

    Add mongo replication

---
+++ docker/mongo/replicaSet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sleep(5000); # Wait for all mongo instances to start
+
+mongo --host mongo1:27017 <<EOF
+rs.initiate({
+    "_id": "rs0",
+    "version": 1,
+    "members": [
+        {
+            "_id": 0,
+            "host": "mongo1:27017",
+        },
+        {
+            "_id": 1,
+            "host": "mongo2:27017",
+        },
+        {
+            "_id": 2,
+            "host": "mongo3:27017",
+        }
+    ]
+});
+EOF
--- public/index.php
+++ public/index.php
@@ -1,5 +1,41 @@
 <?php
 
-// phpinfo();
+$mongoManager = new MongoDB\Driver\Manager(
+    'mongodb://mongo1,mongo2,mongo3/',
+    [
+        'replicaSet' => 'rs0',
+        'readPreference' => 'secondaryPreferred',
+    ]
+);
 
-new MongoDB\Driver\Manager('mongodb://mongodb1,mongodb2,mongodb3/', ['replicaSet' => 'replicaSet']);
+
+if ($_GET['write']) {
+    $writer = new MongoDB\Driver\BulkWrite;
+    $document = [
+        '_id' => new MongoDB\BSON\ObjectID,
+        'string' => 'Test1',
+        'int' => 26700
+    ];
+    $writer->insert($document);
+    $mongoManager->executeBulkWrite('test.testCollection', $writer);
+}
+
+$query = new MongoDB\Driver\Query([]);
+$cursor = $mongoManager->executeQuery('test.testCollection', $query);
+$serverUsed = $cursor->getServer();
+$serverUsedHost = $serverUsed->getHost();
+$serverUsedIsPrimary = $serverUsed->isPrimary();
+$serverUsedIsSecondary = $serverUsed->isSecondary();
+
+echo "<h1>Server used</h1>";
+echo "<h2>Host</h2>";
+var_dump($serverUsedHost);
+echo "<h2>Primary</h2>";
+var_dump($serverUsedIsPrimary);
+echo "<h2>Secondary</h2>";
+var_dump($serverUsedIsSecondary);
+
+echo "<h1>Results</h1>";
+foreach ($cursor as $document) {
+    var_dump($document);
+}
